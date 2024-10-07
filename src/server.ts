27dc9f1da6177bdbd96c5969df2664af75a146cb
Matthew Chen <mattokc35@gmail.com>
2024-10-06 19:27:49 -0500

    test

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Your React app
+    origin: "*", // Your React app
     methods: ["GET", "POST"],
   },
 });
